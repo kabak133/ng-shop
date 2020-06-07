4bee9b1df16dba0cd88bcf98f3070f98e0b5122a
dima-tashchi <d.tashchi@iconic.info>
2020-06-07 19:24:01 +0300

    - init project - some components - routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ProductsListPageComponent} from "./pages/products-list-page/products-list-page.component";
+import {ProductDetailsPageComponent} from "./pages/product-details-page/product-details-page.component";
+import {NotFoundComponent} from "./pages/not-found/not-found.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: ProductsListPageComponent
+  },
+  {
+    path:'product',
+    redirectTo: '',
+    pathMatch: 'full',
+    children:[
+      {
+       path: ":id",
+        component: ProductDetailsPageComponent
+      }
+    ]
+  },
+  {
+    path: "**",
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsListPageComponent } from './pages/products-list-page/products-list-page.component';
+import { ProductDetailsPageComponent } from './pages/product-details-page/product-details-page.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsListPageComponent,
+    ProductDetailsPageComponent,
+    NavbarComponent,
+    SidebarComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
