390689c2d9dfdb29465b3891f7bf657b7b2bb0cc
dima-tashchi <d.tashchi@iconic.info>
2020-06-08 14:25:09 +0300

    - reboot outline, - logo link

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {slideInOut} from "../animations/sliddInOut";
+import {slideInOut} from "../../animations/sliddInOut";
 
 @Component({
   selector: 'app-cart',
